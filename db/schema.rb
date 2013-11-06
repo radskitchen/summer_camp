51799767d2c65836f2420d538b90bb072163bdbe
Chad Kern <radskitchen@gmail.com>
2013-11-05 18:25:42 -0800

    Updated fromt end to new authentication system
    Added simple_form
    Upadated several forms to bootstrap/simple_form
    Added logi/logout/profile links to nav
    Added collectiom select to status form

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,6 @@
 ActiveRecord::Schema.define(:version => 20131105070853) do
 
   create_table "statuses", :force => true do |t|
-    t.string   "name"
     t.text     "content"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
